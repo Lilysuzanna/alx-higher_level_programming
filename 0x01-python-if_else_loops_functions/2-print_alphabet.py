68905b5d641357ecefa3efd6870dbc64678dce19
Lilysuzanna <anyadikesuzette@gmail.com>
2023-01-16 09:02:31 -0800

    print alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-import string
-for c in string.ascii_lowercase:
-    print(f"ASCII for {c} is {ord(c)}")
+for i in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(i), end='')
